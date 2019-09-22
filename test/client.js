694cf51569cb224f5ea59d9c9327793bae16bf8f
nichoth <nichoth@gmail.com>
2019-09-22 16:47:05 -0700

    things start and stop

--- test/client.js
+++ test/client.js
@@ -1,6 +1,14 @@
 var test = require('tape')
+var Server = require('../src/server-side/start-ssb')
+var Client = require('../src/client')
 
+var server = Server()
 test('example', function (t) {
     t.plan(1)
     t.pass('ok')
+})
+
+test('done', function(t) {
+    server.close()
+    t.end()
 })
