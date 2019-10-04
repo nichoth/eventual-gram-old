8f2bc8b33ed2e6b383bcdb8631b4860eb99e577c
nichoth <nichoth@gmail.com>
2019-10-03 22:15:31 -0700

    broken config

--- src/rpc-sbot.js
+++ src/rpc-sbot.js
@@ -2,14 +2,15 @@ var ssbClient = require('ssb-client')
 var manifest = require('../manifest.json')
 var ssbKeys = require('ssb-keys')
 
-
 function start (opts, cb) {
     if (typeof opts === 'function') {
         cb = opts
         opts = {}
     }
+    
     opts.manifest = opts.manifest || manifest
     opts.keys = opts.keys || ssbKeys.generate()
+    opts.port = 9999
     ssbClient(opts, function (err, sbot) {
         if (err) throw err
         cb(err, sbot)
--- src/server-side/start-ssb.js
+++ src/server-side/start-ssb.js
@@ -1,56 +1,19 @@
-// var ssbKeys = require('ssb-keys')
-// var ssbConfig = require('ssb-config')
 var ssbConfig = require('ssb-config/inject')
-// var path = require('path')
 var ssbServer = require('ssb-server')
-// var fs = require('fs')
 
 // @TODO check if global sbot is running and use that if possible
 // @TODO can pass in config here
 function startSSB () {
-    // var {
-    //     // SBOT_SHS,
-    //     // SBOT_SIGN,
-    //     // APP_NAME,
-    //     // NODE_ENV
-    // } = process.env
-    // // console.log('env', SBOT_SHS, SBOT_SIGN, APP_NAME, NODE_ENV)
-
-    // @TODO use passed in config
-    // var appName = NODE_ENV === 'development' ? 'eg-DEV' : undefined
-    // appName = APP_NAME ? APP_NAME : 'test-eg'
-
-    // var opts = {}
-    // if (process.env.NODE_ENV === 'development') {
-    //     opts.caps = {
-    //         shs: SBOT_SHS,
-    //         sign: SBOT_SIGN
-    //     }
-    // }
-
-    // var config = ssbConfig()
-    // var config = ssbConfigInject(appName, opts)
-    // console.log('config', config)
-
-    // var keyPath = path.join(config.path, 'secret')
-    // config.keys = ssbKeys.loadOrCreateSync(keyPath)
-    // error, warning, notice, or info (Defaults to notice)
-    // config.logging.level = 'notice'
 
     ssbServer
         .use(require('ssb-master'))
         .use(require('ssb-gossip'))
         .use(require('ssb-replicate'))
         .use(require('ssb-backlinks'))
-        // .call(null, config)
 
+    // var server = ssbServer(ssbConfig)
     var server = ssbServer(ssbConfig('test-eg', { port: 9999 }))
-
-    // var manifest = server.getManifest()
-    // fs.writeFileSync(
-    //     path.join(ssbConfig.path, 'manifest.json'), // ~/.ssb/manifest.json
-    //     JSON.stringify(manifest)
-    // )
+    // var server = ssbServer(ssbConfig('test-eg'))
 
     return server
 }
